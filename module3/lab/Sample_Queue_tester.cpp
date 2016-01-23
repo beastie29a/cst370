8d17a214d40e542a46b3a41b9c612304b8f42f7e
Robert Contreras <robertcontreras@gmail.com>
2016-01-23 15:35:56 -0800

    Added file operations and comments

--- module3/lab/Sample_Queue_tester.cpp
+++ module3/lab/Sample_Queue_tester.cpp
@@ -1,4 +1,14 @@
+/****************************************
+*
+* Title: CST 370 Lab 3
+* Abstract: Look for characters to indicate footnote
+* Author: Robert Contreras
+* ID: cont5739
+* Date: Tuesday, Jan 19
+*
+****************************************/
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 #include "Queue.h"
@@ -6,22 +16,40 @@ using namespace std;
 int main()
 {
    Queue q1;
-   string parseMe = "This part is the beginning. {This part is the footnote.} This part is the end.";
+   string parseMe;
    bool store = false;
+   ifstream sample("sample.txt");
 
+   // Check if file was opened correctly
+   if (!sample.is_open())
+   {
+      cout << "Unable to open File";
+      exit(1);
+   }
+
+   // Store the first line into a string
+   getline(sample,parseMe);
+
+   // Go through string and look for { to indicate footnote
    for ( char c : parseMe )
    {
       if ( c == '{' || store == true )
       {
+         // Look for end char }
          if ( c == '}' )
             store = false;
+         // Skip processing beginning char
          else if ( c == '{' )
             store = true;
+         // Store footnote chars in a queue
          else
             q1.enqueue(c);
       }
    }
+   // Output the footnote
    q1.display(cout); 
    cout << endl; 
+   // Close the file
+   sample.close();
    return 0;
 }
