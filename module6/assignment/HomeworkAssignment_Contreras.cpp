c06aa93f0d9038cf5ca51332f691c9c840ff137b
Robert Contreras <robertcontreras@gmail.com>
2016-02-15 21:00:17 -0800

    Changed outter CoolSort loop to for loop Added more comments

--- module6/assignment/HomeworkAssignment_Contreras.cpp
+++ module6/assignment/HomeworkAssignment_Contreras.cpp
@@ -10,20 +10,28 @@
 #include <iostream>
 using namespace std;
 
+// Method definitions
 void InsertionSort( int array[], int arraySize );
 void CoolSort( int array[], int arraySize, int gap );
 void printArray( int array[], int arraySize );
 
 int main() {
 
+   // Array for InsertionSort
    int sortArray[] = {2, 5, 6, 4, 10, 9, 8, 1, 10, 5};
+   // Array for CoolSort
    int coolSortArray[] = {62, 83, 18, 53, 7, 17, 95, 86, 47, 69, 25, 28};
    int gapArray[] = {5, 3, 1};
 
+   // Test InsertionSort
+   cout << "InsertionSort() Run:" << endl;
+   cout << "Before: ";
+   printArray(sortArray, 10);
+   cout << "After: ";
    InsertionSort(sortArray, 10);
-
    printArray(sortArray, 10);
 
+   // Test Cool Sort on base case
    cout << endl << "CoolSort() Run:" << endl;
    cout << "H =  ";
    printArray(gapArray, 3);
@@ -35,12 +43,16 @@ int main() {
       printArray(coolSortArray, 12);
    }
 
+   // Run CoolSort on sample case 1
    cout << endl << "Sample Test Case 1:" << endl;
    gapArray[0] = 5;
    gapArray[1] = 3;
    gapArray[2] = 1;
 
    int sampleArray1[] = {2, 5, 6, 4, 10, 9, 8, 1, 10, 5};
+
+   cout << "Before: ";
+   printArray(sampleArray1, 10);
 
    cout << "H =  ";
    printArray(gapArray, 3);
@@ -52,9 +64,14 @@ int main() {
       printArray(sampleArray1, 10);
    }
 
+   // Run CoolSort on sample case 2
    cout << endl << "Sample Test Case 2:" << endl;
    gapArray[1] = 2;
    int sampleArray2[] = {2, 5, 9, 4, 10, 7, 8, 1, 11, 5};
+
+   cout << "Before: ";
+   printArray(sampleArray2, 10);
+
    cout << "H =  ";
    printArray(gapArray, 3);
 
@@ -68,6 +85,7 @@ int main() {
    return 0;
 }
 
+// InsertionSort algorithm
 void InsertionSort( int array[], int arraySize )
 {
    int value, i, j;
@@ -84,26 +102,37 @@ void InsertionSort( int array[], int arraySize )
    }
 }
 
+// Modified InsertionSort to create CoolSort
 void CoolSort( int array[], int arraySize, int gap )
 {
-   int value, h = 0, i, j;
-   while ( h < arraySize)
+   // Extra variable h for outter loop
+   int value, h, i, j;
+
+   // Outter loop to process each element
+   for ( h = 0; h < arraySize ; h++ )
    {
-      for ( i = h ; i < arraySize ; i = i + gap)
+      // Inner loop process elements with gap added
+      for ( i = h ; i < arraySize ; i = i + gap )
       {
+         // Store current value from array
          value = array[i];
+         // Set the couter for the final inner loop
          j = i - gap;
+         // This loop handles the insertion
          while ( j >= 0 && array[j] > value )
          {
+            // Swap values
             array[j + gap] = array[j];
+            // Set j back to the previous location
             j = j - gap;
          }
+         // Set the array value
          array[j + gap] = value;
       }
-      h++;
    }
 }
 
+// Method to print the array
 void printArray(int array[], int arraySize)
 {
    cout << "[";
